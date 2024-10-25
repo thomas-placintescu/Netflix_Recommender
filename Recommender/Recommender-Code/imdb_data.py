798dd78ce644c435d77561d5f3fe93f9a12bfe20
thomas_placintescu <thomas.placintescu@gmail.com>
2024-10-25 14:51:57 +0100

    working batch retrieval imdb

--- Recommender/Recommender-Code/imdb_data.py
+++ Recommender/Recommender-Code/imdb_data.py
@@ -1,5 +1,7 @@
 from imdb import Cinemagoer
 import pandas as pd
+import pickle 
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 class MovieTitlesParser:
     """
@@ -8,6 +10,7 @@ class MovieTitlesParser:
     Attributes:
         file_path (str): The path to the 'movie_titles.csv' file.
         data (list): A list to store parsed movie data.
+        imdb_info (list); A list containing information about movies / tv shows from the imdb database
     """
 
     def __init__(self, file_path: str):
@@ -19,7 +22,6 @@ class MovieTitlesParser:
         """
         self.file_path = file_path
         self.data = []
-        
         
     def parse_line(self, line: str):
         """
@@ -68,15 +70,175 @@ class MovieTitlesParser:
 
         return df
             
-            
+class IMDBDataCollector:
+    """
+    A class to collect and store IMDb data for a list of movies.
+
+    Attributes
+    ----------
+    imdb_info : list
+        A list to store collected IMDb data for each movie.
+    ia : Cinemagoer
+        An instance of the Cinemagoer class to interact with IMDb.
+
+    Methods
+    -------
+    __init__():
+        Initializes the IMDBDataCollector with an empty imdb_info list and a Cinemagoer instance.
+    fetch_movie_data(movie_name, year):
+        Fetches movie data from IMDb for a given movie name and release year.
+    get_imdb(movies_df):
+        Concurrently retrieves IMDb data for movies listed in a DataFrame.
+    """
+
+    def __init__(self):
+        """
+        Initializes IMDBDataCollector with an empty imdb_info list and a Cinemagoer instance.
+        """
+
+        self.imdb_info = [] # List to store IMDb data for each movie
+        self.ia = Cinemagoer() # Initialize Cinemagoer instance once to reuse across methods
+
+    def fetch_movie_data(self, movie_name: str, year: int):
+        """
+        Fetches movie data from IMDb for a given movie name and release year.
+
+        Parameters
+        ----------
+        movie_name : str
+            The title of the movie to search for.
+        year : int
+            The release year of the movie.
+
+        Returns
+        -------
+        dict or None
+            A dictionary containing movie details if found, otherwise None.
+
+        Notes
+        -----
+        - Searches for the movie by title and filters results by release year.
+        - Retrieves detailed information for the first matching movie.
+        """
+        try:
+            # Search for movies matching the given title
+            movies = self.ia.search_movie(movie_name)
+            for movie in movies:
+                # Check if the release year matches
+                if movie.get('year') == year:
+                    movie_id = movie.movieID
+                    movie_details = self.ia.get_movie(movie_id)
+                    # Build a dictionary with relevant movie data
+                    return {
+                        'Title': movie_name,
+                        'Release Year': year,
+                        'Type': movie_details.get('kind'),
+                        'Genres': movie_details.get('genres', []),
+                        'Director': [director.personID for director in movie_details.get('directors', [])],
+                        'IMDB Rating': movie_details.get('rating', []),
+                        'Cast': [actor.personID for actor in movie_details.get('cast', [])]
+                    }
+        except Exception as e:
+            # Handle exceptions (e.g., network errors, data parsing issues)
+            print(f"Error fetching {movie_name} ({year}): {e}")
+
+        return None # Return None if movie not found or an error occurred
+    
+    def get_imdb(self, movies_df: pd.DataFrame, batch_size: int=30, max_num_batches: int = 1e7):
+        """
+        Concurrently retrieves IMDb data for movies listed in a DataFrame.
+
+        Parameters
+        ----------
+        movies_df : pandas.DataFrame
+            DataFrame containing 'Title' and 'ReleaseYear' columns for movies to search on IMDb.
+
+        Notes
+        -----
+        - Uses ThreadPoolExecutor to fetch data concurrently.
+        - Updates the imdb_info list with data for each found movie.
+        - Prints progress updates showing the number of movies found.
+        """
+        # Use ThreadPoolExecutor to fetch data concurrently - set max workers to 10 so that IMDb is not overloaded
+        batch_start_index = 0
+        batch_counter = 0
+        while batch_counter < max_num_batches and batch_start_index < len(movies_df['Title']) - batch_size:
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                # Map each future to its corresponding movie title and year
+                futures = {executor.submit(self.fetch_movie_data, movie_name, year):
+                           (movie_name, year) for movie_name, year in zip(
+                                movies_df['Title'].iloc[batch_start_index:batch_start_index + batch_size],
+                                movies_df['ReleaseYear'].iloc[batch_start_index:batch_start_index + batch_size])}
+                # Process completed futures as they become available
+                for future in as_completed(futures):
+                    movie_name, year = futures[future]  # Retrieve the original movie name and year
+                    try:
+                        result = future.result()  # Get the result of the future
+                        if result:
+                            self.imdb_info.append(result)  # Append the movie data to imdb_info
+                            print(f"Movies Found: {len(self.imdb_info)/ len(movies_df['Title'])}")  # Progress update
+                    except Exception as e:
+                        # Handle exceptions that occurred during data fetching
+                        print(f"Error processing {movie_name} ({year}): {e}")
+            batch_start_index += batch_size
+            batch_counter += 1
+            if batch_counter > max_num_batches:
+                print(f"Reached max num batches {max_num_batches}, so aborting")
+                break
+
+    def get_imdb_non_batched(self, movies_df: pd.DataFrame, limit_download: int = 1e7):
+        """
+        Concurrently retrieves IMDb data for movies listed in a DataFrame.
+
+        Parameters
+        ----------
+        movies_df : pandas.DataFrame
+            DataFrame containing 'Title' and 'ReleaseYear' columns for movies to search on IMDb.
+
+        Notes
+        -----
+        - Uses ThreadPoolExecutor to fetch data concurrently.
+        - Updates the imdb_info list with data for each found movie.
+        - Prints progress updates showing the number of movies found.
+        """
+        # Use ThreadPoolExecutor to fetch data concurrently - set max workers to 10 so that imdb not overloaded
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            # Map each future to its corresponding movie title and year
+            futures = {
+                executor.submit(self.fetch_movie_data, movie_name, year): (movie_name, year)
+                for movie_name, year in zip(movies_df['Title'].iloc[:limit_download], 
+                                            movies_df['ReleaseYear'].iloc[:limit_download])
+            }
+            # Process completed futures as they become available
+            for future in as_completed(futures):
+                movie_name, year = futures[future]  # Retrieve the original movie name and year
+                try:
+                    result = future.result()  # Get the result of the future
+                    if result:
+                        self.imdb_info.append(result)  # Append the movie data to imdb_info
+                        print(f"Movies Found: {len(self.imdb_info)} / {len(movies_df)}")  # Progress update
+                except Exception as e:
+                    # Handle exceptions that occurred during data fetching
+                    print(f"Error processing {movie_name} ({year}): {e}")
+
+if __name__ == "__main__":
+
+    file_path = "/Users/Thomas/Recommender_Netflix/Recommender/Data/movie_titles.csv"
+
+    parser = MovieTitlesParser(file_path)
+    parser.parse_file()
+    movie_titles = parser.get_dataframe()
+
+    #movie_titles.head()
+
+    collector = IMDBDataCollector()
+    collector.get_imdb(movie_titles, max_num_batches = 3)
+    print(collector.imdb_info)
+    # Save results
+    with open('imdb_info.pkl', 'wb') as f:
+        pickle.dump(collector.imdb_info, f)
 
 
-file_path = "/Users/Thomas/Recommender_Netflix/Recommender/Data/movie_titles.csv"
 
-parser = MovieTitlesParser(file_path)
-parser.parse_file()
-movie_titles = parser.get_dataframe()
 
-# Display the first few rows
-movie_titles.info()
 
